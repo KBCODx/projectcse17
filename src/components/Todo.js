ccbc36825a6125b9c1f080047c3853e75c506bbf
KRISHNA BANSAL <tech.krishna18@gmail.com>
2026-06-17 01:29:50 +0530

    new

---
+++ src/components/Todo.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+function Todo() {
+    const [task, setTask] = useState("");
+    const [tasks, setTasks] = useState([]);
+
+    const addTask = () => {
+        if (task.trim() === "") return;
+
+        setTasks([...tasks, task]);
+        setTask("");
+    };
+
+    const deleteTask = (index) => {
+        const updatedTasks = tasks.filter((_, i) => i !== index);
+        setTasks(updatedTasks);
+    };
+
+    return (
+        <div>
+            <h1>Todo App</h1>
+
+            <input
+                type="text"
+                placeholder="Enter a task"
+                value={task}
+                onChange={(e) => setTask(e.target.value)}
+            />
+
+            <button onClick={addTask}>
+                Add
+            </button>
+
+            <ul>
+                {tasks.map((item, index) => (
+                    <li key={index}>
+                        {item}
+
+                        <button onClick={() => deleteTask(index)}>
+                            Delete
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Todo;
